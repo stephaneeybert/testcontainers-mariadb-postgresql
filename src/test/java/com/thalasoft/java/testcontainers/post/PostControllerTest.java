b9bdd18bc55c3d7bb6df7a094b91154ef71ff2db
Stephane Eybert <mittiprovence@yahoo.se>
2024-05-30 15:44:53 +0200

    remove an unused import statement

--- src/test/java/com/thalasoft/java/testcontainers/post/PostControllerTest.java
+++ src/test/java/com/thalasoft/java/testcontainers/post/PostControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
