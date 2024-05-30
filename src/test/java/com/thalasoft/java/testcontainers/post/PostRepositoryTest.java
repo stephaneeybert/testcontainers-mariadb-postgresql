3e59a5bae523274a582a17d54e3170bc472deed6
Stephane Eybert <mittiprovence@yahoo.se>
2024-05-30 12:12:14 +0200

    use JUnit 5

--- src/test/java/com/thalasoft/java/testcontainers/post/PostRepositoryTest.java
+++ src/test/java/com/thalasoft/java/testcontainers/post/PostRepositoryTest.java
@@ -6,28 +6,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import com.thalasoft.java.testcontainers.TestContainerConfig;
-
-@Testcontainers
-@DataJdbcTest
-@Import(TestContainerConfig.class)
-class PostRepositoryTest {
-
-    @Autowired
-    MariaDBContainer<?> mariaDBContainer;
-
-    @Autowired
-    PostgreSQLContainer<?> postgreSQLContainer;
-
-    @Autowired
-    PostRepository postRepository;
+import org.junit.jupiter.api.Test;
 
     @Test
     void mariadbConnectionEstablished() {
